be156c8f8162a1621412e15b2020a7e60799ff7a
Liyou Zhou <leothemagnificent@gmail.com>
2013-10-14 17:48:59 +0100

    fix bug in function calls; add default param to class

--- solarcell.py
+++ solarcell.py
@@ -10,36 +10,37 @@ q = 1.602176565 * 10 ** -19 # electron charge
 
 class solarcell(object):
 
-    def __init__(self, ):
-        self.D1 = diode( 1, 10**-14 )
-        self.D2 = diode( 2, 10**-12 )
-        self.Rs = 0.1
-        self.Rsh = 1e6
+    def __init__(self, Rs=0.1, Rsh=1e6, n2=2, I01=10**-14, I02=10**-12):
+        self.n1 = 1
+        self.n2 = n2
+        self.I01 = I01
+        self.I02 = I02
+        self.D1 = diode( self.n1, self.I01 )
+        self.D2 = diode( self.n2, self.I02 )
+        self.Rs = Rs
+        self.Rsh = Rsh
         self.T = 300;
+        self.I_L = 0;
 
-    def f(I):
+    def f(self, I):
         V_D = V - I * self.Rs
-        y = I_L - self.D1.current(V_D,self.T) - \
+        y = self.I_L - self.D1.current(V_D,self.T) - \
                   self.D2.current(V_D,self.T) - \
                   V_D/self.Rsh - I
         return y
 
-    def df(I):
+    def df(self, I):
         V_D = V - I * self.Rs
         y = -((q*self.Rs)/(k*self.T)) * ( self.D1.current(V_D,self.T) +
-                                self.D2.current(V_D,self.T) ) - \
-            self.Rs/self.Rsh - 1
+                                          self.D2.current(V_D,self.T) ) \
+            - self.Rs/self.Rsh - 1
         return y
 
     def current(self, V, T, I_L):
+        self.I_L = I_L
         global k,q
-        self.T = T
-
-        try:
-            I = scipy.optimize.newton(f, -0.1, fprime=df)
-        except RuntimeError:
-            I = 0
-
+        try: I = scipy.optimize.newton(self.f, -0.1, fprime=self.df)
+        except RuntimeError: I = 0
         return I
 
     def calibrate(self, data):
